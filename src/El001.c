314b981589e1c6204ceb901b411e1ce31a9b788f
michele olivieri <micheleolivieri02@gmail.com>
2024-11-14 13:32:55 +0100

    El001.c solution implementation

--- src/El001.c
+++ src/El001.c
@@ -16,13 +16,34 @@
 
 void main() {
 	// Le stringhe da ordinare (array di puntatori)
-	char* strings[] = { "prova","1","adfadfas","bsfufdasi","ciao" };
+	char* strings[] = { "prova","1","michele","31","bianca","ciao","nicolò" };
 	// Il numero di stringhe nell'array
 	int num = sizeof(strings) / sizeof(strings[0]);
 
 	__asm{
-		mov ecx, num
-		
+			MOV ECX, num			; ECX = i, indice del ciclo esterno
+			DEC ECX
+
+		i_loop:
+			XOR EAX, EAX 			; EAX = j, indice del ciclo interno
+
+		j_loop:
+			MOV ESI, [strings + EAX*4]		; carica strings[j] in ESI
+			MOV EDI, [strings + EAX*4 + 4]	; carica strings[j+1] in EDI
+			MOV BL, BYTE PTR [ESI]			; carica il primo carattere della stringa 
+			MOV BH, BYTE PTR [EDI]			; carica il primo carattere della stringa
+			CMP BL, BH
+			JLE no_swap				; esegue lo sccambio se strings[j][0] <= strings[j+1][0]
+
+			MOV [strings + EAX*4], EDI		; scambia strings[j] con strings[j+1]
+			MOV [strings + EAX*4 +4], ESI	; scambia strings[j+1] con strings[j]
+
+		no_swap:
+			INC EAX					; incrementta j
+			CMP EAX, ECX			; if (j < i)
+			JL j_loop				; rientra nel ciclo
+
+			LOOP i_loop
 	}
 
 	// Stampa su video
