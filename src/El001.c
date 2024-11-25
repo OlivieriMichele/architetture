cc1f7de59656bd33da9c765dc173ae76965e0ef5
michele olivieri <micheleolivieri02@gmail.com>
2024-11-25 18:55:08 +0100

    fix El001.c

--- src/El001.c
+++ src/El001.c
@@ -16,7 +16,7 @@
 
 void main() {
 	// Le stringhe da ordinare (array di puntatori)
-	char* strings[] = { "prova","1","michele","31","bianca","ciao" };
+	char* strings[] = { "prova","1","michele","31","bianca","ciao","pizza" };
 	// Il numero di stringhe nell'array
 	int num = sizeof(strings) / sizeof(strings[0]);
 
@@ -26,19 +26,25 @@ void main() {
 
 		i_loop:
 			XOR EAX, EAX 			; EAX = j, indice del ciclo interno
+			XOR EDX, EDX			; indice usato per accedere alla lettera della stringa
+
+		next: 
+			INC EDX
 
 		j_loop:
 			MOV ESI, [strings + EAX*4]		; carica strings[j] in ESI
 			MOV EDI, [strings + EAX*4 +4]	; carica strings[j+1] in EDI
-			MOV BL, BYTE PTR [ESI]			; carica il primo carattere della stringa 
-			MOV BH, BYTE PTR [EDI]			; carica il primo carattere della stringa
+			MOV BL, [ESI][EDX]				; carica il primo carattere della stringa 
+			MOV BH, [EDI][EDX]				; carica il primo carattere della stringa
 			CMP BL, BH
-			JLE no_swap				; esegue lo sccambio se strings[j][0] <= strings[j+1][0]
+			JL no_swap				; non esegue lo sccambio se strings[j][0] < strings[j+1][0]
+			JE next					; confronta le seconde lettare in caso di ugualianza della prima
 
 			MOV [strings + EAX*4], EDI		; scambia strings[j] con strings[j+1]
 			MOV [strings + EAX*4 +4], ESI	; scambia strings[j+1] con strings[j]
 
 		no_swap:
+			XOR EDX, EDX
 			INC EAX					; incrementta j
 			CMP EAX, ECX			; if (j < i)
 			JL j_loop				; rientra nel ciclo
