c10e54b6b952a3d7d6fd6a1a1f03d765f1c53fd4
michele olivieri <micheleolivieri02@gmail.com>
2025-01-27 15:39:43 +0100

    last version

--- src/El003.c
+++ src/El003.c
@@ -16,74 +16,71 @@
 void main()
 {
     // Input
-    unsigned char vet[] = {0x4E,0x8B,0x8A,0x0A,0xDE,0x0B,0x46,0x85,0xD6,0x9F,0x17,0x88,0x01};
+    unsigned char vet[] = {0x5D,0xC4,0x8A,0x81,0xEA,0xC5,0xE4,0x76,0x8D,0x93,0x01,0x4C,0x4B,0x75,0x47,0x49,0x07,0x71,0x15,0xEC,0xDB,0x1F,0xDC,0xD4,0xCA};
     unsigned int len = 104;	// lunghezza (numero di bit)
-    unsigned char n = 25;	// numero di bit dati
+    unsigned char n = 99;	// numero di bit dati
 
     // Ouput
     unsigned char errori = 0;	// 1 = errori; 0 = no errori
 
     __asm{
-        xor edi, edi                ; indice del vettore che analizzo
-        xor esi, esi                ; contatore per il ciclo esterno
+		xor edi, edi				; indice (byte) del vettore che voglio analizzare
+		xor esi, esi				; contatore per il ciclo esterno
+		xor ecx, ecx
 
-    _ciclo:
-        xor edx, edx                ; registro che conterrà i miei elementi
-        xor ecx, ecx
-        mov cl, n
-        mov edi, esi
-        shr edi, 3                  ; edi = esi / 8 indice del vettore che contiene il bit di parità
-        mov ebx, 1                  ; maschera di bit
-        xor eax, eax                ; contenitore per la somma di uni
-        mov dh, vet[edi+3]
-        mov dl, vet[edi+2]
-        shl edx, 16
-        mov dh, vet[edi+1]
-        mov dl, vet[edi]
-        mov edi, esi
-        and edi, 7                  ; edi = resto della divisione per 8 equivale alla posizione del bit di parità
-        
-    _shift_mask:                    ; aggiorna la maschera in funzione della posizione del bit di parità
-        cmp edi, 0
-        jle _fine_shift
-        shl ebx, 1
-        dec edi
+	_ciclo: 
+		xor edx, edx 				; registro contenitore del byte da analizzare
+		xor ebx, ebx				; contatore dei giri interni del ciclo
+		xor eax, eax				; contenitore per la somma di uni
+
+	_reload:
+		mov cx, si					; cx = bx / 8 = indice del byte che voglio esaminare
+        add cx, bx
+		shr cx, 3
+		mov dl, vet[ecx]            ; metto in dx la word che voglio esamniare
+        mov dh, vet[ecx+1]
+        mov cx, si
+        add cx, bx
+        and cx, 7                   ; calcolo il resto della divisione per 8 = indice del bit da controllare
+    _shift_mask:
+        cmp cx, 0                   ; porta nel bit meno significativo il primo bit da controllare
+        jle _next
+        shr dx, 1
+        dec cx
         jmp _shift_mask
-    _fine_shift:
+	_next:
+		test dl, 1					; controllo il bit meno significativo
+		jz _not_inc
+		inc eax
+	_not_inc:
+		shr dl, 1					; analizza il bit successivo
+		inc ebx
+		cmp bl, n
+		jge _exit					; esce quando ha processato n bit
+		mov cx, si
+        add cx, bx
+		and cx, 7					; controllo il resto della divisione per 8
+		jz _reload					; se ho esaminato 8 bit carico il byte successivo
+		jmp _next
+	_exit:
 
-    _inner_loop:
-        dec cl                      ; indice del cilto interno, esegue n confronti
-        test edx, ebx               ; controlla il bit del vettore in corrispondenza della maschera
-        jz _not_inc
-        inc eax                     ; incrementa la somma di uni
-    _not_inc:
-        shl ebx, 1                   ; sposta la maschera a sinistra
-        cmp cl, 0
-        jne _inner_loop
+		and ax, 1 					; considero solo il bit meno significativo che rappresenta la parità
+		and dl, 1					; considero solo il bit meno significatico che è il bit di parità
+		cmp al, dl
+		jne _errore
 
-    _shift_loop:                    ; posiziona il bit di parità nel bit meno significativo del byte
-        shr edx, 1
-        shr ebx, 1
-        cmp ebx, 1
-        jg _shift_loop
+		xor ecx, ecx
+		mov cl, n
+		inc cl						; esamino il successivo blocco di n+1 elementi
+		add esi, ecx
+		cmp esi, len
+		jl _ciclo
+		jmp _fine
 
-        ; ora basta controllare che il bit meno significativo di eax corrisponda al successivo bit della sequenza
-        and ax, 1
-        and dl, 1
-        cmp al, dl                  ; esegue il confronto tra il bit di parità e il bit meno significativo di ax
-        jne _errore
-        
-        mov cl, n                  ; ripristino ecx = n
-        inc cl
-        add esi, ecx                ; continua a processare i successvi n (ecx) bit
-        cmp esi, len
-        jl _ciclo
-        jmp _fine
-
-    _errore:
-        mov errori, 1
-
-    _fine:
+	_errore:
+		mov errori, 1
+		
+	_fine:
     }
 
     // Stampa su video
